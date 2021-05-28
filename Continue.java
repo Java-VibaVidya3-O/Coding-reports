e55512a75764ee0dfeb2aac194914ac6010182a0
Kartik Gaddam <56345834+Karthick986@users.noreply.github.com>
2021-05-28 19:44:01 +0530

    Delete Continue.java

--- Continue.java
+++
@@ -1,9 +0,0 @@
-public class Continue {
-    public static void main(String[] args) {
-
-        for (int i=1; i<=20; i++) {
-            if (i>=11 && i<=15) continue;
-            System.out.println(i);
-        }
-    }
-}
